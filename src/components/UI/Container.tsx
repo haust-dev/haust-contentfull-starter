2327560b318ffd7b9b15af428b731d8943806b47
Adi Delibasic <dev@haust.io>
2023-07-05 21:33:40 +0200

    container children props

--- src/components/UI/Container.tsx
+++ src/components/UI/Container.tsx
@@ -3,8 +3,9 @@ import { GetFormData, GetImageBySlugData } from '@/types/contentfulTypes'
 import Image from 'next/image'
 import { createGetContainedImageQuery } from '@/queries/getContainedImage'
 import Link from 'next/link'
+import { ReactNode } from 'react'
 
-export function Container({ children }) {
+export function Container({ children }: { children: ReactNode }) {
   const slug = 'logo'
   const { loading, error, data } = useQuery<GetImageBySlugData>(
     createGetContainedImageQuery(slug),
@@ -14,7 +15,6 @@ export function Container({ children }) {
   if (error) return <p></p>
   if (!data) return <p>Something went wrong...</p>
   const image = data.containedImageCollection.items[0].image
-  console.log(image)
   return (
     <section className="my-4 px-4">
       <div className="mx-auto max-w-4xl">
