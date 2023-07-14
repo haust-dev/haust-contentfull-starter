78569c3163e9f5835131113d9c6a499b53756aa1
Adi Delibasic <dev@haust.io>
2023-07-14 17:06:04 +0200

    back button

--- src/components/UI/Container.tsx
+++ src/components/UI/Container.tsx
@@ -18,15 +18,23 @@ export function Container({ children }: { children: ReactNode }) {
   return (
     <section className="my-4 px-4">
       <div className="mx-auto max-w-4xl">
-        <div className="relative w-52 h-32 text-center mx-auto mb-4">
-          <Link href="/">
-            <Image
-              src={image.url}
-              style={{ objectFit: 'contain' }}
-              alt={image.title}
-              fill
-            />
-          </Link>
+        <div className="mb-4">
+          <div className="relative flex justify-between items-center w-full">
+            <div className="uppercase font-medium">
+              <p>Back to homepage</p>
+            </div>
+            <div className="relative w-20">
+              <Link href="/">
+                <Image
+                  src={image.url}
+                  style={{ objectFit: 'contain' }}
+                  alt={image.title}
+                  height={image.height}
+                  width={image.width}
+                />
+              </Link>
+            </div>
+          </div>
         </div>
         <div>{children}</div>
       </div>
